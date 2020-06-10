e82c89df3441d3e8c9592037c89dde95cb2f15bc
bks5a79 <brandon_k_sides@homedepot.com>
2020-06-10 15:53:10 -0500

    Added hello world

---
+++ 1-HelloWorld/HelloWorld.py
@@ -0,0 +1,36 @@
+# You made it!  Welcome to Python!
+
+# You are probably wondering what you're looking at.  A Python file, denoted by the .py extension, is simply a text file
+# structured in a way that the Python interpreter knows how to read.  Notice the pound signs (or octothorpes, or number
+# signs, or hashtags, or whatever terminology you prefer) at the beginning of each of these lines.  These pound signs
+# tell the Python interpreter to ignore these lines, so I don't have to worry about what I type here; the interpreter
+# won't read it, so I can say whatever I want!  Hey, Python interpreter!  You're ugly!  Don't worry, it can't hear.
+
+# Comments are a good way to make your code understandable to others, and that's how I'll be using them here.  So how
+# about we talk about the line right below this one - the one that doesn't start with a '#'
+
+print("This is a print statement!")
+
+# THAT is a line of bona-fide python code, and when you run this file, that's what the interpreter is reading.  We've
+# just told Python to take the phrase we gave it - "This is a print statement!" - and "print" it to the terminal.  Let's
+# break it down a little further.
+
+# there are three parts of this line I want us to pay attention to: the word 'print', the parentheses, and the quoted
+# part of the line.
+
+# 'print' is the name of a function in Python.  You don't have to worry about what a "function" is just yet - that will
+# come later.  For now, just think of them as a kind of command.  There are many functions built into python, and you
+# can also define functions yourself.  When we want to use a function in python code, we "call" that function.
+
+# Calling a function is done by typing the name of the function, followed by a pair of parentheses.  Sometimes just the
+# pair of parentheses is enough; for instance, the print statement below is perfectly valid:
+print()
+# but this just prints an empty line.  We want to print a specific phrase.
+
+# For this reason, function calls take "arguments" - extra bits of information that modify the function's behavior.
+# These "arguments" go in between the parentheses.  That's what we're doing with our first print statement: We are
+# calling the "print" function, with the phrase "This is a print statement!" as an argument!
+
+# Now for an exercise: add an additional print statement below these comments, and have it print "Hello, world!"
+
+# Or have it print whatever you like!  It's your program, after all!
---
+++ 1-HelloWorld/README.md
@@ -0,0 +1,20 @@
+# Hello, World!
+
+It's time for everyone's favorite programming exercise: the hello world program!  You'll write a program that prints
+`Hello, world!` to the terminal!
+
+`HelloWorld.py` is a file that contains a python program.  You can run it on your own machine by opening your terminal,
+`cd`ing into the directory containing it, and running the following command (not including the preceding `$`):
+
+```
+$ python3 HelloWorld.py
+```
+
+You will see the following two lines, one of which should be empty, appear in your terminal:
+```
+This is a print statement!
+
+```
+
+Let's take a look at how this is working.  To do so, go ahead and open HelloWorld.py.  I'll explain more once you're
+there!
